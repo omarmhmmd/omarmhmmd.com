af5e339ea23d8e12d02fc546a938e29b2c18fbf0
omarmhmmd <omar.mhmmd@gmail.com>
2025-01-15 15:02:29 -0800

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site: 'https://omarmhmmd-com.vercel.app/',
+  integrations: [
+    mdx(), 
+    sitemap(
+
+  )],
 });
