c9a1f8e6866f34b9f4363f4468edca420c8fc257
omarmhmmd <omar.mhmmd@gmail.com>
2025-08-20 18:29:26 -0700

    added entries and edited css 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://omarmhmmd-com.vercel.app/',
+  site: 'https://omarmhmmd.com/',
   integrations: [
     mdx(),
     sitemap(
